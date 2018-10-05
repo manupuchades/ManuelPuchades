e4622873c7dac962a8dc8a316a4a0051fc81ed5d
manupuchades <m.puchades@gmail.com>
2018-10-05 12:56:34 +0200

    class C52Test.java

---
+++ src/test/java/es/upm/miw/SVC/C52Test.java
@@ -0,0 +1,13 @@
+package es.upm.miw.SVC;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class C52Test {
+    @Test
+    void mATest()
+    {
+        assertEquals( "mA",new C52().mA());
+    }
+}
