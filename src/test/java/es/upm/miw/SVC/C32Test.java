67867127f6ed56580e9786acdd5d5f36c1f66487
manupuchades <m.puchades@gmail.com>
2018-10-05 12:12:53 +0200

    class C32Test.java

---
+++ src/test/java/es/upm/miw/SVC/C32Test.java
@@ -0,0 +1,13 @@
+package es.upm.miw.SVC;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class C32Test {
+    @Test
+    void mATest()
+    {
+        assertEquals( "mA",new C32().mA());
+    }
+}
