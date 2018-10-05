626a2a9dad93c9cd29ec1a805ba82b0c31bc99c8
manupuchades <m.puchades@gmail.com>
2018-10-05 12:07:13 +0200

    class C31Test.java

---
+++ src/test/java/es/upm/miw/SVC/C31Test.java
@@ -0,0 +1,18 @@
+package es.upm.miw.SVC;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class C31Test {
+    @Test
+    void m1Test()
+    {
+        assertEquals( "m1",new C31().m1());
+    }
+    @Test
+    void m2Test()
+    {
+        assertEquals("m2",new C31().m2());
+    }
+}
