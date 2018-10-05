0b8eb2ccba22b2019c1abed8c29026bf08fc23af
manupuchades <m.puchades@gmail.com>
2018-10-05 12:26:43 +0200

    class C51Test.java

---
+++ src/test/java/es/upm/miw/SVC/C51Test.java
@@ -0,0 +1,19 @@
+package es.upm.miw.SVC;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class C51Test {
+    @Test
+    void m1Test()
+    {
+        assertEquals( "m1",new C51().m1());
+    }
+
+    @Test
+    void m2Test()
+    {
+        assertEquals( "m2",new C51().m2());
+    }
+}
